11f13370d9ba3c654f25ed359e32b332f5c18015
aldhubaiban <47508708+aldhubaiban@users.noreply.github.com>
2019-10-07 12:48:44 -0400

    Add files via upload

---
+++ USF/java/project0/CorrectCode.java
@@ -0,0 +1,14 @@
+/* “I pledge my Honor that I have not cheated, and will not cheat, on this assignment.”*/
+public class CorrectCode
+{
+   public static void main(String [] args)
+   {
+     System.out.println("This program is riddled" + " with syntax errors.");
+     System.out.println("But if you can output"+" this message,");
+     System.out.println("then you have corrected them all!");           
+   }
+
+}
+/* there was syntax errors that makes code not working, 
+So I fixed the errors to make the code works */
+
